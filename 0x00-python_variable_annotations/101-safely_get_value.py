1e9d890caea740ecc63e32e54c91bc6a69adcc61
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2023-01-05 21:36:07 +0300

    advanced

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,7 +9,7 @@ def safely_get_value(
         dct: Mapping,
         key: Any,
         default: Union[T, None] = None
-        ) -> Union[Any, T]:
+) -> Union[Any, T]:
     """ More  annotations """
     if key in dct:
         return dct[key]
